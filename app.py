33272d5ffed12b9414c4281c0a52556ca40317ba
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2019-04-01 22:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
 
-    data = "<h1>Welcome!</h1><br><pr>Nothing much to check in here go to /ist/[rollno] for info on the student<br>Eg:/ist/180101001<br>You can use this API for purposes related to Attendence or just info purpose,use it with an app to get images and stuff, think what to do later<br><br><br>PS:I am lazy to design this page :3<br><br><br><br><br><br><br><br><br><br><br><br><br><br>Made By: Nyzex</p>"
+    data = "<h1>Welcome!</h1><br><pr>Nothing much to check in here go to /ist/[rollno] for info on the student<br>Eg:/ist/180101001<br>You can use this API for purposes related to Attendence or just info purpose,use it with an app to get images and stuff, think what to do later<br><br><br>PS:I am lazy to design this page :3<br><br><br><br><br><br><br><br><br><br><br><br><br><br>Made By: Nyzex<br><br><br>DataFeed: JordenPiu (Yet to be added)</p>"
     return data
 
 @app.route('/ist/<string:rollno>')
